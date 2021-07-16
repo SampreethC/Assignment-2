16ebb5a2a544f4644e749c52aeb10c7b44feda14
SampreethC <87109576+SampreethC@users.noreply.github.com>
2021-07-16 22:58:31 +0530

    Add files via upload

---
+++ Replace.js
@@ -0,0 +1,28 @@
+//5. search for a particular text and replace it with new word
+//6. write it back on file
+//7. display again on the screen
+
+var glob = require('glob');
+var fs = require('fs');
+var replace = require('replace');
+
+// Find file(s)
+glob('exe-1.txt', function(err, files) {
+    if (err) { throw err; }
+    files.forEach(function(item, index, array) {
+          console.log(item + ' found');
+          // Read file
+          console.log(fs.readFileSync(item,'utf8'));
+          // Find and Replace string
+          replace({
+              regex: 'See',
+              replacement: 'Saw',
+              paths: [item],
+              recursive: true,
+              silent: true
+          });
+          console.log('Replacement complete');
+          // Read file
+          console.log(fs.readFileSync(item,'utf8'));
+      });
+});
