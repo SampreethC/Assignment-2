f2f3c292bc65ccba9752292d6ef0ae1456878040
SampreethC <87109576+SampreethC@users.noreply.github.com>
2021-07-16 23:01:25 +0530

    Add files via upload

---
+++ Append.js
@@ -0,0 +1,20 @@
+//8. if the file with text written is already present then append text to next line
+
+//append
+
+var fs=require("fs");
+
+try
+{
+    fs.appendFile('exe1.txt','See U Later...',function(err) 
+    {
+        if(err)
+        {
+            return console.log(err);
+        }
+    });
+}
+catch(err)
+{
+    console.log(err);
+}
