600ea6ab12401280f9983dea4973aa4e2169983e
SampreethC <87109576+SampreethC@users.noreply.github.com>
2021-07-21 13:20:21 +0530

    Add files via upload

---
+++ FileCreate&Display.js
@@ -0,0 +1,42 @@
+//2. if file not present create it
+//3. write some text in it
+//4. display the text on screen
+
+var fs = require('fs');
+var checkFileExist=require("./FileCheck");
+
+
+if (checkFileExist('example_file.txt'))
+{
+	console.log('File does exist');
+}
+else
+{
+	console.error('File does not exist');
+
+		// Create the file and write some text in it
+		console.log('\nCreating the file');
+		fs.writeFileSync("example_file.txt", "Hello world!");
+
+				//Display the text on screen
+				fs.readFile('example_file.txt',function(err,data) 
+        		{
+        			if(err)
+    				{
+        				return console.log(err);
+    				}
+    				console.log("File data: "+data.toString());
+				});
+
+}
+
+
+/*
+Output:
+
+is not readable
+File does not exist
+
+Creating the file
+File data: Hello world!
+*/
